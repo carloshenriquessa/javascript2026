9949f331a20ba33e4dbf4c60761c375d7d0712f1
Carlos Henrique <carloshenrique4516@gmail.com>
2026-02-12 16:03:32 -0300

    Laços de repetição

--- aulas-js/exemplo01/js/index.js
+++ aulas-js/exemplo01/js/index.js
@@ -1,19 +1,6 @@
-function verificaCor() {
-    let cor = document.getElementById("cor").value;
-    cor = cor.toLowerCase()
+const carros = [" Gol", " Fusca", " Brasilia", " Del Rey", " Chevette"];
+var tamanho = carros.length;
 
-    switch (cor) {
-        case "azul":
-            document.body.style.backgroundColor = "blue";
-            break;
-        case "vermelho":
-            document.body.style.backgroundColor = "red";
-            break;
-        case "verde":
-            document.body.style.backgroundColor = "green";
-            break;
-        default:
-            document.getElementById("teste").innerHTML = "Nenhuma cor disponivel para " + cor;
-    }
+for (let i = 0; i < tamanho; i++){
+    document.getElementById("teste").innerHTML += carros[i] + " - ";
 }
-
