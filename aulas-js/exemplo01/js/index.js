4e45d18d40a49ae7866a58c1152bd8f2e48959d3
Carlos Henrique <carloshenrique4516@gmail.com>
2026-02-16 15:28:01 -0300

    .

--- aulas-js/exemplo01/js/index.js
+++ aulas-js/exemplo01/js/index.js
@@ -1,6 +1,17 @@
-const carros = [" Gol", " Fusca", " Brasilia", " Del Rey", " Chevette"];
-var tamanho = carros.length;
+function ativarContagem(){
+    //document.getElementById('tempo').innerHTML = "Começou a contar!";
+    //tempo = setTimeout(function(){ 
+        //document.body.style.backgroundColor = "blue";
+     //}, 5000);
 
-for (let i = 0; i < tamanho; i++){
-    document.getElementById("teste").innerHTML += carros[i] + " - ";
+     tempo = setInterval(function(){  
+        var cronometro = document.getElementById("tempo").innerHTML;
+        var soma = parseInt(cronometro) + 1;
+        document.getElementById('tempo').innerHTML = soma;
+       }, 1000)
+}
+
+function pararContagem(){
+    clearInterval(tempo);
+    //document.getElementById('tempo').innerHTML = "Parou a contagem!";
 }
